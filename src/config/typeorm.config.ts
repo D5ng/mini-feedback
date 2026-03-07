e2ed3db85e7bbb3125c67833ebfbd2cc09c66111
d5ng <dong5ffice@gmail.com>
2026-03-07 23:38:30 +0900

    feat: TypeORM 설정 추가

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,19 @@
+import { registerAs } from '@nestjs/config'
+import { TypeOrmModuleOptions } from '@nestjs/typeorm'
+import { DataSource, DataSourceOptions } from 'typeorm'
+
+const config: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: `${process.env.DB_HOST}`,
+  port: parseInt(`${process.env.DB_PORT}`, 10),
+  username: `${process.env.DB_USERNAME}`,
+  password: `${process.env.DB_PASSWORD}`,
+  database: `${process.env.DB_DATABASE}`,
+  entities: ['dist/**/**/*.entity{.ts,.js}'],
+  migrations: ['dist/migrations/*{.ts,.js}'],
+  autoLoadEntities: true,
+  synchronize: false,
+}
+
+export default registerAs('typeorm', () => config)
+export const connectionSource = new DataSource(config as DataSourceOptions)
