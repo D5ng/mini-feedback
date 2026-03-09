e0f29984a8a87842f7c69aaf962829941b0b2371
d5ng <dong5ffice@gmail.com>
2026-03-10 00:18:26 +0900

    feat: refine AppController and enhance Swagger documentation setup

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import cookieParser from 'cookie-parser'
 import { Logger } from 'nestjs-pino'
 import { AppModule } from './app.module'
@@ -13,6 +14,22 @@ async function bootstrap() {
 
   app.use(cookieParser())
   app.useLogger(app.get(Logger))
+
+  const config = new DocumentBuilder()
+    .setTitle('API Experimental')
+    .setDescription('API Experimental 서비스의 REST API 문서입니다.')
+    .setVersion('1.0.0')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'Authorization',
+      in: 'header',
+    })
+    .build()
+
+  const swaggerDocument = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('swagger-ui', app, swaggerDocument)
 
   app.useGlobalInterceptors(new BaseResponseInterceptor())
   app.useGlobalFilters(new GlobalExceptionFilter())
